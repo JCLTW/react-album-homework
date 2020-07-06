096ed930ee8ce48b9dc7cc66d396ee3e4d711d2f
Jiancheng LI <jiancheng.li@thoughtworks.com>
2020-07-07 00:44:55 +0800

    feat, create userInfo component

--- src/App/__tests__/App.test.js
+++ src/App/__tests__/App.test.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from '../App';
+import UserInfo from '../../UserInfo/UserInfo';
 
 test('should render App', () => {
   const { getByTestId } = render(<App />);
   const app = getByTestId('app');
 
   expect(app).not.toBeEmpty();
+});
+
+test('should render UserInfo', () => {
+  const { getByTestId } = render(<UserInfo />);
+  const userInfo = getByTestId('user-info');
+
+  expect(userInfo).not.toBeEmpty();
 });
--- src/UserInfo/UserInfo.js
+++ src/UserInfo/UserInfo.js
@@ -3,7 +3,38 @@ import './UserInfo.scss';
 
 class UserInfo extends Component {
   render() {
-    return <aside className="UserInfo">User Info</aside>;
+    const { name, email, phone, website, company } = this.props.user;
+
+    return (
+      <aside className="UserInfo" data-testid="user-info">
+        <h1>{name}</h1>
+        <div className="sub-title">Contract Me@</div>
+        <div className="row">
+          <label>Email</label>
+          <div data-testid="email" className="data highlight">
+            {email}
+          </div>
+        </div>
+        <div className="row">
+          <label>Phone</label>
+          <div data-testid="phone" className="data highlight">
+            {phone}
+          </div>
+        </div>
+        <div className="row">
+          <label>Website</label>
+          <div data-testid="website" className="data highlight">
+            {website}
+          </div>
+        </div>
+        <div className="row">
+          <label>Company</label>
+          <div data-testid="company" className="data">
+            {company && company.name}
+          </div>
+        </div>
+      </aside>
+    );
   }
 }
 
